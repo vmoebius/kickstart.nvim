a6bf7e2968b577b4cbd735dded2af435b0e550c9
Volker Moebius <vmoebius@gmail.com>
2024-02-15 22:28:51 +0100

    adapted netrw behavior

--- init.lua
+++ init.lua
@@ -93,6 +93,10 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
+vim.g.netrw_banner = 0
+vim.g.netrw_browse_split = 4
+vim.g.netrw_winsize = 25
+
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
