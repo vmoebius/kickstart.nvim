20d76d1dbba69431e79035ef779cf834d79e133d
Volker Moebius <vmoebius@gmail.com>
2026-05-19 23:06:52 +0200

    adapted netrw behavior

--- init.lua
+++ init.lua
@@ -101,6 +101,10 @@ do
   -- Set to true if you have a Nerd Font installed and selected in the terminal
   vim.g.have_nerd_font = false
 
+  vim.g.netrw_banner = 0
+  vim.g.netrw_browse_split = 4
+  vim.g.netrw_winsize = 25
+
   -- [[ Setting options ]]
   --  See `:help vim.o`
   -- NOTE: You can change these options as you wish!
